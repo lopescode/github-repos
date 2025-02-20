172920f82c65bff9f448a1c90659098f07c3f25b
Lopes <leonardolopesb97@hotmail.com>
2025-02-19 23:44:25 -0300

    Fetching repos

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,58 @@
+import { ChangeEvent, useCallback, useState } from "react";
+import { FaGithub, FaPlus } from "react-icons/fa";
+import api from "../services/api";
+
 const Main = () => {
+  const [newRepo, setNewRepo] = useState<string>();
+  const [repositories, setRepositories] = useState<string[]>([]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewRepo(e.target?.value);
+  };
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent) => {
+      e.preventDefault();
+
+      try {
+        const response = await api.get(`/repos/${newRepo}`);
+
+        setRepositories([...repositories, response.data.full_name]);
+        setNewRepo("");
+      } catch (error) {
+        console.log(error);
+      }
+
+      console.log(repositories);
+    },
+
+    [newRepo, repositories]
+  );
+
   return (
-    <div>
-      <h1 className="text-red-500">Main</h1>
+    <div className="flex flex-col gap-10 bg-white m-[80px] p-[30px] rounded-sm max-w-[700px]">
+      <h1 className="flex justify-center items-center gap-2 text-xl">
+        <FaGithub size={25} />
+        My Repositories
+      </h1>
+
+      <form
+        method="post"
+        onSubmit={handleSubmit}
+        className="flex justify-center items-center gap-2"
+      >
+        <input
+          type="text"
+          placeholder="Add a repository"
+          className="p-2 border border-gray-300 rounded-sm w-full"
+          value={newRepo}
+          onChange={(e) => handleInputChange(e)}
+        />
+
+        <button type="submit" className="bg-[#0D2636] p-2 border rounded-sm">
+          <FaPlus color="#FFF" size={20} />
+        </button>
+      </form>
     </div>
   );
 };
